44536752aa33477114ffbf565721570a0f4042cf
goohooh <goo_oo@naver.com>
2019-02-04 15:41:03 +0900

    implement reduce function

--- basic.js
+++ basic.js
@@ -21,17 +21,25 @@ function take(length, iter) {
     return res;
 }
 
+function reduce(f, acc, iter) {
+    for (const a of iter) {
+        acc = f(acc, a);
+    }
+    return acc;
+}
+
+const add = (a, b) => a + b;
+
 // 리스트에서 홀수를 length만큼 뽑아서 제곱한 후 더하기
 function f(iter, length) {
-    let acc = 0;
-    for (const a of take(length, map(a => a * a, filter(a => a % 2, iter)))) {
-        acc += a;
-    }
+    // 명령형 프로그래밍에서 if, 값, 증가, for문 같이 추상화 된것을
+    // FP에선 함수를 이용해 같은 동작을 한다.
+    return reduce(add, 0, take(length, map(a => a * a, filter(a => a % 2, iter))));
 
     // 함수 내부는 외부 세상에 영향을 끼치지 않는다.
     // log 함수 호출은 main에서 실행
     // 수학적 프로그래밍이 되도록 함수에선 return
-    return acc;
+    // return acc;
 }
 
 function main() {
